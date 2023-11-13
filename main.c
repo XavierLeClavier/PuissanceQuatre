d4a9858ca12640502dc26d2fac8670ae2f3ebd10
Xavi <xavier.stoa@gmail.com>
2023-11-13 15:36:48 +0100

    Ajout verification de victoire horizontale et verticale (il faut refaire horizontale)

--- main.c
+++ main.c
@@ -1,49 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 int main()
 {
-    char columns[8][1] = {'1', '2', '3', '4', '5', '6', '7', '\0'}; // columns
-    char L1[8][1] = {'_', '_', '_', '_', '_', '_', '_', '\0'};
-    char L2[8][1] = {'_', '_', '_', '_', '_', '_', '_', '\0'};
-    char L3[8][1] = {'_', '_', '_', '_', '_', '_', '_', '\0'};
-    char L4[8][1] = {'_', '_', '_', '_', '_', '_', '_', '\0'};
-    char L5[8][1] = {'_', '_', '_', '_', '_', '_', '_', '\0'};
-    char L6[8][1] = {'_', '_', '_', '_', '_', '_', '_', '\0'};
-    char arrayoflines[7][8][1] = {columns, L1, L2, L3, L4, L5, L6};
+    char* columns = "1234567"; // columns
+    char L1[] = "_______";
+    char L2[] = "_______";
+    char L3[] = "_______";
+    char L4[] = "_______";
+    char L5[] = "_______";
+    char L6[] = "_______";
+    char* templine;
+    char arrayoflines[7] = {columns, L1, L2, L3, L4, L5, L6};
 
     bool play = true;
     int column, line;
     char player1[20], player2[20];
 
-    printf("Bienvenue dans le jeu du puissance 4 !\n");
-    printf("Joueur 1, quel est votre nom ?\n");
+    printf("Bienvenue dans le jeu du puissance 4 !\nJoueur 1, quel est votre nom ?\n");
     scanf("%s", player1);
     printf("Joueur 2, quel est votre nom ?\n");
     scanf("%s", player2);
-    printf("Bienvenue %s et %s !\n", player1, player2);
+    printf("Bienvenue %s et %s !\n%s sera représenté par des X et %s par des O\n", player1, player2, player1, player2);
 
     while (play = true)
     {
-        system("clear");
+        // system("clear");
         printf("Voici le plateau de jeu :\n");
         for (int i = 0; i < 7; i++)
         {
-            for (int j = 0; j < 8; j++)
-            {
-                printf("%s", arrayoflines[i][j]);
-                printf(" ");
-            }
-            printf("\n");
+            templine = arrayoflines[i];
+            printf("%s\n", templine);
         }
 
+        printf("%s, dans quelle colonne voulez-vous jouer ?\n", player1);
+        scanf("%d", &column);
+        column = column - 1;
+        line = 5;
+        while (arrayoflines[line][column] != '_') //to find the first empty line
+        {
+            line = line - 1;
+        }
+        arrayoflines[line][column] = 'X';
 
+        //same for player 2
+        printf("%s, dans quelle colonne voulez-vous jouer ?\n", player2);
+        scanf("%d", &column);
+        column = column - 1;
+        line = 5;
+        while (arrayoflines[line][column] != '_') 
+        {
+            line = line - 1;
+        }
+        arrayoflines[line][column] = 'O';
 
-        play = false;
+        //check if there is a winner...
+        
+        //...in the columns
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (arrayoflines[j][i] == arrayoflines[j+1][i] && arrayoflines[j][i] == arrayoflines[j+2][i] && arrayoflines[j][i] == arrayoflines[j+3][i])
+                {
+                    if (arrayoflines[j][i] == 'X')
+                    {
+                        printf("Bravo %s, vous avez gagné !\n", player1);
+                        play = false;
+                    }
+                    else if (arrayoflines[j][i] == 'O')
+                    {
+                        printf("Bravo %s, vous avez gagné !\n", player2);
+                        play = false;
+                    }
+                }
+            }
+        }
+
+        //...in the lines
+        if (play == true)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arrayoflines[j][i] == arrayoflines[j][i+1] && arrayoflines[j][i] == arrayoflines[j][i+2] && arrayoflines[j][i] == arrayoflines[j][i+3])
+                    {
+                        if (arrayoflines[j][i] == 'X')
+                        {
+                            printf("Bravo %s, vous avez gagné !\n", player1);
+                            play = false;
+                        }
+                        else if (arrayoflines[j][i] == 'O')
+                        {
+                            printf("Bravo %s, vous avez gagné !\n", player2);
+                            play = false;
+                        }
+                    }
+                }
+            }
+        }
     }
-
     return 0;
 }
